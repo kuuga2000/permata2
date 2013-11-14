f6277bb3249616f5260c9fd382dabd3b1ad431e1
Kuuga <k_kuuga@yahoo.com>
2013-11-14 09:38:04 +0700

    Product Detail Price
    Product Detail Price

--- !permata/application/site/views/product_detail.php
+++ !permata/application/site/views/product_detail.php
@@ -66,7 +66,21 @@
 			<div class="fleft nett-price">
 				<div class="title">PRICE</div>
 				<span class="currency">IDR</span>
-				<span class="price1"><?php echo substr($disc, 0, strlen($disc)-3);?></span>
+				<span class="price1">
+				<?php 
+						$countCaracterPrice=strlen(substr(number_format($disc, 0, '', '.'), 0, strlen($disc) - 2));
+						if($countCaracterPrice==4){
+						?>
+						
+						<?php echo implode('', explode('.', substr(number_format($disc, 0, '', '.'), 0, strlen($disc) - 2)));?>
+					    
+					    <? }elseif($countCaracterPrice==5){ ?>
+					    	<?php echo implode('.', explode('.', substr(number_format($disc, 0, '', '.'), 0, strlen($disc) - 2)));?>
+					    <? }elseif($countCaracterPrice==3){ ?>
+					    	<?php echo implode('', explode('.', substr(number_format($disc, 0, '', '.'), 0, strlen($disc) - 2)));?>
+					    <? } ?>	
+					<?php //echo substr($disc, 0, strlen($disc)-3);?>
+				</span>
 				<span class="price2">.<?php echo substr($disc, -3);?></span>
 				<div class="cb"></div>
 			</div>
